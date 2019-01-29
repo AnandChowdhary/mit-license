59b097a3d2d55aef4f40e4e2faf4b18488eb753b
Anand Chowdhary <mail@anandchowdhary.com>
2019-01-29 22:45:28 +0100

    Add Netlify build

--- twente.me/index.ts
+++ twente.me/index.ts
@@ -1,9 +1,33 @@
 const fs = require("fs");
 const path = require("path");
+const fileName = require("file-name");
+const mustache = require("mustache");
+const yaml = require("js-yaml");
+
 const people = path.join(__dirname, "..", "..", "people");
 
-console.log("Starting Twente.me static site generator!");
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
+const template = fs
+  .readFileSync(
+    path.join(__dirname, "..", "..", "twente.me", "template.mustache")
+  )
+  .toString();
 
 fs.readdirSync(people).forEach(file => {
-  console.log(file);
+  const pagePath = path.join(__dirname, "..", "site", `${fileName(file)}.html`);
+  ensureDirectoryExistence(pagePath);
+  try {
+    const yamlString = yaml.safeLoad(
+      fs.readFileSync(path.join(people, file)).toString()
+    );
+    fs.writeFileSync(pagePath, mustache.render(template, yamlString));
+  } catch (e) {}
 });
