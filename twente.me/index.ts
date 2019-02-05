9c0c521b507f04596b2161525674c8f4ded4360b
Anand Chowdhary <mail@anandchowdhary.com>
2019-02-05 20:28:40 +0100

    Work on a better hosted page

--- twente.me/index.ts
+++ twente.me/index.ts
@@ -4,6 +4,10 @@ const fileName = require("file-name");
 const mustache = require("mustache");
 const yaml = require("js-yaml");
 const copy = require("copy");
+const axios = require("axios");
+const serial = require("promise-serial");
+const dotenv = require("dotenv");
+dotenv.config();
 
 import { getTemplate } from "./utils/template";
 import { getLicense } from "./utils/license";
@@ -23,15 +27,76 @@ people.forEach(person => {
   );
   ensureDirectoryExistence(pagePath);
   try {
-    const yamlString: String = yaml.safeLoad(
+    const yamlString: any = yaml.safeLoad(
       fs
         .readFileSync(path.join(process.env.NODE_PATH || ".", "people", person))
         .toString()
     );
-    fs.writeFileSync(
-      pagePath,
-      mustache.render(template, { ...yamlString, license })
-    );
-  } catch (e) {}
-  copy(path.join(process.env.NODE_PATH || ".", "twente.me", "static", "*"), path.join(process.env.NODE_PATH || ".", "dist", "site"), () => {});
+    ["scripts", "styles"].forEach(key => {
+      if (
+        yamlString.hasOwnProperty(key) &&
+        typeof yamlString[key] === "object" &&
+        yamlString[key].length
+      ) {
+        for (let i = 0; i < yamlString[key].length; i++) {
+          const obj: any = {};
+          obj[key] = yamlString[key][i];
+          yamlString[key][i] = {
+            httpString: obj
+          };
+        }
+      }
+    });
+    const promises = [];
+    const fetchedRepos = [];
+    if (
+      yamlString.hasOwnProperty("repos") &&
+      typeof yamlString.repos === "object" &&
+      yamlString.repos.length
+    ) {
+      yamlString.repos.forEach(repo =>
+        promises.push(
+          () =>
+            new Promise((resolve, reject) => {
+              axios
+                .get("https://api.github.com/repos/" + repo, {
+                  headers: {
+                    "User-Agent": "AnandChowdhary",
+                    Authorization: "token " + process.env.GITHUB_TOKEN
+                  }
+                })
+                .then(response => {
+                  fetchedRepos.push(response.data);
+                  resolve();
+                })
+                .catch(error => {
+                  console.log("Got an error", error);
+                  reject();
+                });
+            })
+        )
+      );
+    }
+    serial(promises)
+      .then(() => {})
+      .catch(() => {})
+      .then(() => {
+        fs.writeFileSync(
+          pagePath,
+          mustache.render(template, {
+            ...yamlString,
+            license,
+            repositories: fetchedRepos
+          })
+        );
+        console.log(fetchedRepos);
+      });
+  } catch (e) {
+    console.log("Error", e);
+  }
+  copy(
+    path.join(process.env.NODE_PATH || ".", "twente.me", "static", "*"),
+    path.join(process.env.NODE_PATH || ".", "dist", "site"),
+    () => {}
+  );
 });
